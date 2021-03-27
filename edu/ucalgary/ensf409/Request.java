1e8736f76a0df68b6ced4dc0664299056d7e8d72
MatteoPM <mmmatteo8@gmail.com>
2021-03-27 13:04:52 -0600

    added Request class to store user input

---
+++ edu/ucalgary/ensf409/Request.java
@@ -0,0 +1,25 @@
+package edu.ucalgary.ensf409;
+
+public class Request {
+	private static String type;
+	private static String furniture;
+	private static int quantity;
+	
+	public Request(String t, String f, int q){
+		type = t;
+		furniture = f;
+		quantity = q;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public String getFurniture() {
+		return furniture;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+}
