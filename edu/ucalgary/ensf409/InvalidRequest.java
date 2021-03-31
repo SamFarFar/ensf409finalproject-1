89b2c4cdf2c5eebba8a835b3eae66c3d449c5f0a
Sandip Mishra <sandipm02@icloud.com>
2021-03-30 20:32:26 -0600

    Fixing

--- edu/ucalgary/ensf409/InvalidRequest.java
+++ edu/ucalgary/ensf409/InvalidRequest.java
@@ -1,24 +1,20 @@
 package edu.ucalgary.ensf409;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.sql.*;
 
 public class InvalidRequest {
+    
     private Connection dbConnect;
     private ResultSet results;
 
     public InvalidRequest(String dburl, String user, String pass) {
-        
         StringBuffer manufacture = new StringBuffer();
-
         // connect to database
         try{
             dbConnect = DriverManager.getConnection(dburl, user, pass);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
+        
         // create map manufacturers
         try {
             Statement myStmt = dbConnect.createStatement();
@@ -30,32 +26,7 @@ public class InvalidRequest {
             myStmt.close();
         } catch (Exception e) {
             e.printStackTrace();
-        }
-
-        // create file
-        try {
-            File myObj = new File("OrderForm.txt");
-            if (myObj.createNewFile()) {
-                System.out.println("Invalid Request: Suggested Orderform Created");
-            } 
-          } catch (IOException e) {
-            System.out.println("Error #3: Could Not Create File");
-            e.printStackTrace();
-          }
-
-          // write out to file
-          try {
-            FileWriter myWriter = new FileWriter("OrderForm.txt");
-            myWriter.write("Invalid Furniture Request\n");
-            myWriter.write('\n');
-            myWriter.write("List of Manufactures:\n");
-            // connect to database and print out list of manufactures
-            myWriter.write(manufacture.toString());
-            myWriter.close();
-          } catch (IOException e) {
-            System.out.println("Error #4: Order Form Error Could Not Be Completed");
-            e.printStackTrace();
-          }
+        } 
     }
     
 }
